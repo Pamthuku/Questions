0d17b09927b46ce0968d9d3873b73d1c627e9278
Pamthuku <marbamdhi@gmail.com>
2021-01-25 13:54:41 -0800

    Questions1

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'questions.dart';
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IAlertDialog(),
+    );
+  }
+}
+
+class IAlertDialog extends StatefulWidget {
+  @override
+  _IAlertDialogState createState() => _IAlertDialogState();
+}
+
+class _IAlertDialogState extends State<IAlertDialog> {
+  List<Icon> myAnswer = [];
+  int close = 0;
+  int check = 0;
+
+  QA qa = new QA();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(20, 20, 25, 0.8),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(20, 20, 25, 1.0),
+        shadowColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Questions",
+            style: TextStyle(
+                color: Color.fromRGBO(255, 200, 0, 1.0),
+                fontWeight: FontWeight.bold,
+                fontSize: 25),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    qa.questions[qa.qNum].getquest(),
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: myAnswer,
+            ),
+            ansbt(yes: true, no: false, click: "1"),
+            SizedBox(
+              height: 10,
+            ),
+            ansbt(yes: false, no: true, click: "2"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  FlatButton ansbt({bool yes, bool no, String click}) {
+    return FlatButton(
+      shape: Border.all(style: BorderStyle.solid, color: Colors.amber[500]),
+      splashColor: Colors.white,
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      color: Color.fromRGBO(20, 20, 30, 0.7),
+      onPressed: () {
+        setState(() {
+          qa.qNum++;
+        });
+        if (qa.questions[qa.qNum].getuAnswer() == yes) {
+          myAnswer.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+              size: 40,
+            ),
+          );
+
+          check++;
+        } else if (qa.questions[qa.qNum].getuAnswer() == no) {
+          myAnswer.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+              size: 40,
+            ),
+          );
+
+          close++;
+        } else if (qa.questions[qa.qNum].getuAnswer() == null) {
+          Alert(
+            closeIcon: Icon(
+              Icons.close,
+              color: Colors.amber,
+            ),
+            style: AlertStyle(
+              backgroundColor: Color.fromRGBO(20, 20, 30, 0.8),
+              alertBorder: Border.all(
+                  style: BorderStyle.solid, color: Colors.amber, width: 1.5),
+              titleStyle: const TextStyle(
+                color: Color.fromRGBO(255, 200, 0, 1.0),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            context: context,
+            type: AlertType.none,
+            title:
+                "U & I\n$close ခု(အယူအဆအားဖြင့်)ကွဲပြားပြီး\n$check ခု တူညီပါတယ်\n.  .  .\nအစကနေ ပြန်စမည်လား",
+            //desc: "\n",
+            buttons: [
+              DialogButton(
+                color: Color.fromRGBO(20, 20, 30, 1.0),
+                border: Border.all(
+                    style: BorderStyle.solid,
+                    width: 1.5,
+                    color: Color.fromRGBO(255, 200, 0, 1.0)),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 7.0),
+                  child: Text(
+                    "ပြန်စမယ်",
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 200, 0, 1.0),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                onPressed: () => Navigator.pop(context),
+                width: 120,
+              )
+            ],
+          ).show();
+          close = 0;
+          check = 0;
+          qa.qNum = 0;
+          myAnswer.clear();
+        }
+      },
+      child: Text(
+        '$click',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+          color: Color.fromRGBO(255, 200, 0, 1.0),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/questions.dart
@@ -0,0 +1,63 @@
+class Questions {
+  String _ques;
+  bool _uAnswer;
+
+  Questions({String ques, bool uAnswer}) {
+    this._ques = ques;
+    this._uAnswer = uAnswer;
+  }
+  String getquest() {
+    return _ques;
+  }
+
+  bool getuAnswer() {
+    return _uAnswer;
+  }
+}
+
+class QA {
+  int qNum = 0;
+  List<Questions> questions = [
+    new Questions(
+        ques:
+            '1. အချိန်ဆိုတာ...?\n\n 1 = ဘဝ၏တန်ဖိုး\n2 = ဘဝကိုမှီပြီးဖြစ်ပေါ်လာတဲ့ တကယ်မရှိတဲ့အရာတစ်ခု',
+        uAnswer: false),
+    new Questions(
+        ques:
+            '2. ပုခုံးနှစ်ဖက်ကြား ခေါင်းပေါက်တာချင်းတူရုံနဲ့ "လူချင်းအတူတူပဲ" လို့ ဆိုလို့ရနိုင်ပါသလား...?\n\n 1 = မရနိုင်ပါဘူး\n2 = အတူတူပါပဲ',
+        uAnswer: false),
+    new Questions(
+        ques:
+            '3. လူနဲ့တိရိစ္ဆာန်ကို မည်သည့်အရာနဲ့ ခွဲခြားပါသလဲ...?\n\n 1 = ကိုယ်ချင်းစာတရားနဲ့\n2 = မိုးကိုကျောခိုင်းတာ,မိုးကိုကျောမခိုင်းတာနဲ့ or ဦးနှောက်ပါတာ,မပါတာနဲ့',
+        uAnswer: true),
+    new Questions(
+        ques:
+            '4. ကိုယ်ချင်းစာတရားရှိရုံနဲ့ လူအစစ်ဖြစ်ပါပြီလား...?\n\n 1 = အဲ့လောက်ဆိုဖြစ်မယ်ထင်တယ်\n2 = လူအစစ်တော့ မဖြစ်သေးပါဘူး',
+        uAnswer: true),
+    new Questions(
+        ques:
+            '5. မည်သည့်အချိန်မှာ လူအစစ်ရယ်လို့ဖြစ်ပါသလဲ...?\n\n 1 = အမှန်နဲ့အမှား ကွဲပြားသွားတဲ့အချိန်ကစပြီး\n2 = လူစိတ်ပေါက်နေတဲ့အချိန်အခိုက်အတန့်မှာ',
+        uAnswer: false),
+    new Questions(
+        ques:
+            '6. အမောရဲ့ဟိုဘက်မှာ မည်သည့်အရာရှိပါသလဲ...?\n\n 1 = သေတာရှိတယ်\n2 = မမောတာရှိတယ်',
+        uAnswer: true),
+    new Questions(
+        ques:
+            '7. ယောကျ်ားတွေရဲ့ အလှတရားက မည်သည့်ဥစ္စာပါလဲ...? \n\n1 = ကာယဗလ ပိုက်ဆံ \n2 = အရည်အချင်း',
+        uAnswer: false),
+    new Questions(
+        ques: '8. မိန်းမတွေရဲ့အလှတရားဆိုတာ...?\n\n1 = ယဥ်ကျေးမှု \n2 = ရုပ်ရည်',
+        uAnswer: false),
+    new Questions(
+        ques:
+            'မေးခွန်းအားလုံး ဖြေဆို၍ပြီးပါပြီ၊ \n\nအဖြေမှန်\n?1 == 2 \n?2 == 1 \n?3 == 1 \n?4 == 2 \n?5 == 1 \n?6 == 2 \n?7 == 2 \n?8 == 1',
+        uAnswer: true),
+    new Questions(ques: null, uAnswer: null)
+  ];
+  void qnum() {
+    if (qNum < questions.length) {
+      qNum++;
+    }
+  }
+}
